16bf1b5b5d0bbbe06346c97598393794e585da37
naheemtaj44 <naheemtaj44@gmail.com>
2024-08-16 14:19:43 +0500

    add python code

---
+++ Lecture_01/first.js
@@ -0,0 +1,104 @@
+// console.log("apna collage")
+// fullname="durra baloch";
+// console.log(fullname);
+
+// age=22
+// console.log(age);
+
+
+// isfollow= false
+// console.log(isfollow);
+// let name="naeem taj";
+// console.log(name);
+// totalPrice=1000;
+// console.log(totalPrice);
+// let age =21;
+// console.log(age);
+
+// #before 2015 usage var
+// var age1=23;
+// var age1=33;
+// var age1=12;
+// console.log(var);
+
+// const name = "duraar";
+// console.log(name);
+
+
+// let a;
+// console.log(a);
+// const a=12;
+// // console.log(a);                           const must be iniallaze let do not need iniallaze
+
+// {
+//     let a = 12;
+//     console.log(a);
+    
+// }
+// {
+//     let a = 11;
+//     console.log(a);
+// }
+
+// let a = 21;
+
+// let age = 22;
+// fullName = "naeemtaj";
+// isFollow= false;
+
+// let x;
+// let s=null;
+
+// const student = {
+//     fullName :"naeem taj",
+//     age: 21,                       
+//     mark: 401,
+//     isPass: true,
+// }
+
+// student["age"] =student["age"] +33;
+// console.log(student["age"]);
+// console.log(student.mark);
+
+// #practice question
+
+const product={
+    tittle:"ballpen",
+    rating: 4,
+    price:270,
+    offer:5,
+
+}
+product["price"]=product["price"]+12;
+console.log(product["price"])
+
+
+
+
+
+
+
+
+
+
+
+student = {
+    fullName : "Naeem Taj",
+    RollNumber : 2225530,
+    obtainMark : 401,
+    totalMark : 501,
+    collage :"D9 campus", 
+}
+
+student["totalMark"] = student["totalMark"]+49;
+console.log(student["totalMark"])
+
+ const profile = {
+    fullName : "naheem taj",
+    post : 195,
+    followers : "569K",
+    following : 4,
+
+}
+console.log(typeof profile["fullName"]);
+
---
+++ Lecture_02/main.js
@@ -0,0 +1,146 @@
+//console.log("naheem")
+
+
+//arithmatic operation
+// let a =10;
+// let b = 2;
+// console.log(" a =",a ," and  b =",b);
+// console.log(" a + b =",a+b)
+// console.log(" a - b =",a-b)
+// console.log(" a / b =",a/b)
+// console.log(" a * b =",a*b)
+// console.log(" a % b =",a%b)
+// console.log(" a ** b =",a**b)      //exponential
+
+//unary operator
+// let a =10;
+// let b = 2;
+// console.log(" a =",a ," and  b =",b);
+// // a= a +1; //11
+// // a++;
+// a--;
+// a=a-1;
+// console.log("a=",a);//11
+// let a =10;
+//  let b = 2;
+
+//  console.log(" a =",a ," and  b =",b);
+// //  console.log("++a=",++a);//11
+// console.log("a++=",a++);//10
+// console.log("a=",a);//11
+
+// let d=12;
+// let f = 2;
+// console.log("d =",d,"& f =",f);
+// console.log("d + f",d + f);
+// console.log("d - f",d - f);
+// console.log("d * f",d * f);
+// console.log("d / f",d / f);
+// console.log("d % f",d % f);
+// console.log("d ** f",d ** f);
+
+//Assignment operator +=, -=, *=, %=,/=
+
+// let a =10;
+//  let b = 2;
+//  a*=5;
+//  console.log("a =",a);
+
+ //comprision operator
+//  let a = 12;
+//  let b = "5";
+//  console.log("12==5",a==b); //false
+// console.log("12!=5",a!=b);
+// console.log("a!==b",a!==b);
+// console.log("12>=5",a>=b);
+
+
+//logical operator
+// let a = 9;
+// let b = 7;
+// let con1= a > b;
+// let con2 = a!==b;
+// console.log("con1 || con2",a>b || a==b);
+// console.log("!(a<b)",!(a<b));
+
+//conditional statement
+// let age =14;
+// if(age>18){
+//     console.log("yoy can vote");
+// }
+
+// if(age<18){
+//     console.log("you can not vote")
+// }
+
+// let mode="light";
+// let color;
+// if(mode==="dark"){
+//     color="black";
+// }
+// if(mode==="light"){
+//     color="white";
+// }
+
+// console.log(color);
+
+// let age =14;
+// if(age>18){
+//    console.log("yoy can vote");
+// }
+// else{
+//     console.log("yoy CANNOT vote");
+
+// }
+
+// let num =13;
+// if(num%2===0){
+//     console.log(num, "is even");
+        
+//     }
+//     else{
+//         console.log(num, "is odd")
+    // }
+
+// let mark =40;
+
+//      if (mark>85); {
+//         console.log("A+ grade");
+//     }
+//     if_else(mark >65);{
+//         console.log("B grade")
+//     }
+    
+//     else {
+//     console.log("fail");
+// }
+
+
+// let mode= "dark";
+// let color;
+// if(mode==="dark"){
+//     color="dark"
+// }
+// else if(mode==="blue");{
+//     color="blue";
+// }else if(mode==="red");{
+//     color="red";
+// } else {
+//     color="white";
+// }
+
+//ternary operator
+// let age= 12;
+
+// let result= age> 10 ? "adult":"teenager";
+// console.log(result);
+
+// let age= 12;
+//  age> 15 ? console.log("adult"):console.log("teenager");
+
+let number
+//  alert("hell0")
+  number=prompt("enter ur number");
+  if(number%5===0);{
+    console.log("multiple of five",number);
+  }
