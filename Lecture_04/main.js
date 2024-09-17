ecbe23c87dfe9624ad7fda856c75de340437dd69
naheemtaj44 <naheemtaj44@gmail.com>
2024-09-17 23:26:59 +0500

    add task

---
+++ Lecture_04/main.js
@@ -0,0 +1,36 @@
+// //Array in js
+// let names=["naeem","taj","balach","qamber","babag"]
+// console.log(names)
+
+// marks=[43,55,78,99,98,67,54]
+// console.log(marks)
+// //array indices
+// marks[2]
+// marks[4]=33;
+
+// let marks= [12,44,78,99,76];
+// console.log(marks.length);
+// let name=["naeem","taj","balach","qamber","babag"]
+// console.log(name.length);
+
+// let age=[12,43,43,3443,43,44];
+// console.log(age.length);
+// let subject=["naeem","taj","balach","qamber"]
+
+// let marks=[12,15,67,88,98,76,99,76];
+// for(let i=0; i< marks.length; i++);{
+// console.log(marks[i]);}
+
+//looping over is arrays
+// let i=0;
+// let names=["name","taj","balach","qamber","babag"];
+// // for(let i=0; i< names.length; i++);{
+// // console.log(names[i]);}
+// for(let name of names){
+//     console.log(name);
+// }
+
+marks=["naeem","taj","balach","qamber","babag"];
+for(let mark of marks){
+    console.log(mark.toUpperCase());
+}
